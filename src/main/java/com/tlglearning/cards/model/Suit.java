ea03744d3250b2d4f0f9b959e09f3415afabe4fc
dsk0057 <dkafle6141@gmail.com>
2022-07-29 15:59:16 -0400

    Initial commit

---
+++ src/main/java/com/tlglearning/cards/model/Suit.java
@@ -0,0 +1,19 @@
+package com.tlglearning.cards.model;
+
+public enum Suit {
+
+  CLUBS('\u2663'),
+  DIAMONDS('\u2662'),
+  HEARTS('\u2661'),
+  SPADES('\u2660');
+
+  private final char symbol;   // No setter because of final
+
+  Suit(char symbol) {
+    this.symbol = symbol;
+  }
+
+  public char getSymbol() {
+    return symbol;
+  }
+}
